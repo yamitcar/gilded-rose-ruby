45a4aa22d713016b2fd24351d54c7eccbf41022d
kleer <kleer@kleer.ala>
2016-06-22 12:07:38 -0500

    adjust runner

--- app.rb
+++ app.rb
@@ -13,15 +13,14 @@ items.push Item.new "Backstage passes to a TAFKAL80ETC concert", 5, 49
 #this conjured item does not work properly yet
 items.push Item.new "Conjured Mana Cake", 3, 6
 
-app = GildedRose.new 
+app = GildedRose.new
 
 days = 2
-(0..days).each {|i| 
+(0..days).each {|i|
   puts "------------ day #{i} ---------"
   puts "name, sell_in, quality"
   for item in items
     puts item
   end
-  app.updateQuality items
+  app.update_quality items
 }
-
